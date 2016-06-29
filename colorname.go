8ae2587d845064889654adbafa95f90e309b1507
John Colagioia <john@colagioia.net>
2016-06-29 07:24:43 -0400

    Read colors

--- colorname.go
+++ colorname.go
@@ -1,7 +1,21 @@
 package main
 
-//import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	)
 
 func main() {
+	dat, err := ioutil.ReadFile("allcolor.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	sdat := string(dat)
+	clines := strings.Split(sdat, "\n")
+	for i := 0; i < len(clines); i++ {
+		fmt.Println(clines[i])
+	}
 }
 
