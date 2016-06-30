029e7f1fb85d9c1fd3ea2b3ea1aa359a0d83d6df
John Colagioia <john@colagioia.net>
2016-06-30 06:20:00 -0400

    Split input lines

--- colorname.go
+++ colorname.go
@@ -15,6 +15,10 @@ func main() {
 	sdat := string(dat)
 	clines := strings.Split(sdat, "\n")
 	for i := 0; i < len(clines); i++ {
+		cparts := strings.Split(clines[i], ",")
+		if len(cparts) < 8 {
+			break
+		}
 		fmt.Println(clines[i])
 	}
 }
