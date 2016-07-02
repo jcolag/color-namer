4b5b58e376556f8c64f1dbd14e5d84ec2a4b6326
John Colagioia <john@colagioia.net>
2016-07-02 06:20:35 -0400

    Add parsing to color class

--- colorname.go
+++ colorname.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 	)
 
@@ -15,6 +16,16 @@ type color struct {
 	hue	byte
 	sat	byte
 	val	byte
+}
+
+func (c *color) populateFromRgb(rgb string) {
+	rr, _ := strconv.ParseUint(rgb[0:2], 16, 8)
+	gg, _ := strconv.ParseUint(rgb[2:4], 16, 8)
+	bb, _ := strconv.ParseUint(rgb[4:6], 16, 8)
+	c.rgb = "#" + rgb
+	c.red = byte(rr)
+	c.green = byte(gg)
+	c.blue = byte(bb)
 }
 
 func main() {
